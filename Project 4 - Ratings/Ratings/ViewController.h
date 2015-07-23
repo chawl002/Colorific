c3d404378880e070b0ebe0866da4b1bc523d8d29
User1 <chawl002@ucr.edu>
2015-07-23 14:50:11 -0700

    Started new Ray Wenderlich tutorial

---
+++ Project 4 - Ratings/Ratings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ratings
+//
+//  Created by User1 on 7/23/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Project 4 - Ratings/Ratings/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ratings
+//
+//  Created by User1 on 7/23/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
